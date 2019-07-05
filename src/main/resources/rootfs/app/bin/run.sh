8e072e1edd53497f1fea3e6f3db23ed4d7a56530
Marat Abrarov <abrarov@gmail.com>
2019-07-06 02:01:34 +0300

    Application configuration

--- src/main/resources/rootfs/app/bin/run.sh
+++ src/main/resources/rootfs/app/bin/run.sh
@@ -2,32 +2,39 @@
 
 exit_code=0
 
-j2 --import-env="" "/app/templates/standalone-openshift.xml.j2" -o ${JBOSS_HOME}/standalone/configuration/@base-image.jboss.configuration.filename@
+echo "Preparing application configuration at ${APPLICATION_CONFIGURATION_FILE}"
+j2 --import-env="" "/app/template/application.properties.j2" -o "${APPLICATION_CONFIGURATION_FILE}"
+exit_code=$?
+if [[ ${exit_code} -ne 0 ]]; then
+    echo "Failed to prepare application configuration at ${APPLICATION_CONFIGURATION_FILE}"
+    echo "Exiting with ${exit_code}"
+    exit ${exit_code}
+fi
+
+echo "Preparing JBoss EAP configuration at ${JBOSS_CONFIGURATION_FILE}"
+j2 --import-env="" "/app/template/standalone.xml.j2" -o "${JBOSS_CONFIGURATION_FILE}"
+exit_code=$?
+if [[ ${exit_code} -ne 0 ]]; then
+    echo "Failed to prepare JBoss EAP configuration at ${JBOSS_CONFIGURATION_FILE}"
+    echo "Exiting with ${exit_code}"
+    exit ${exit_code}
+fi
+
+"@base-image.cmd@" $@ &
+jboss_pid=$!
 exit_code=$?
 
-if [[ ${exit_code} -ne 0 ]]; then
-    echo "Failed to fill JBoss EAP configuration template"
-else
-    echo "Filled JBoss EAP configuration template, starting JBoss EAP..."
+# Forward stop signals to JBoss EAP process
+trap "kill -TERM ${jboss_pid}" INT
+trap "kill -QUIT ${jboss_pid}" QUIT
+trap "kill -TERM ${jboss_pid}" TERM
 
-    "@base-image.cmd@" $@ &
-    jboss_pid=$!
-    exit_code=$?
+# todo: monitor deployment status, use jboss_pid to monitor status of JBoss EAP process
 
-    # Forward stop signals to JBoss EAP process
-    trap "kill -TERM ${jboss_pid}" INT
-    trap "kill -QUIT ${jboss_pid}" QUIT
-    trap "kill -TERM ${jboss_pid}" TERM
-
-    # todo: monitor deployment status, use jboss_pid to monitor status of JBoss EAP process
-
-    wait ${jboss_pid}
-    trap - TERM QUIT INT
-    wait ${jboss_pid}
-    exit_code=$?
-
-    echo "JBoss EAP stopped with ${exit_code}"
-fi
+wait ${jboss_pid}
+trap - TERM QUIT INT
+wait ${jboss_pid}
+exit_code=$?
 
 echo "Exiting with ${exit_code}"
 exit ${exit_code}
