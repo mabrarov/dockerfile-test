e40512cfa1c1f5a0fb1d5e2f2b1059cb611a9ad6
Marat Abrarov <abrarov@gmail.com>
2019-08-02 12:39:09 +0300

    Replaced double square brackets with single square brackets to keep launch script compatible with POSIX shell

--- hollow-image/src/main/resources/rootfs/app/bin/run.sh
+++ hollow-image/src/main/resources/rootfs/app/bin/run.sh
@@ -26,16 +26,16 @@ function int() {
 
 function split_by_colon {
   array="${1}"
-  if [[ "${array}" = "" ]]; then
+  if [ "${array}" = "" ]; then
     return
   fi
   number_of_params="${#}"
   from_index_defined=0
   till_index_defined=0
-  if [[ "${number_of_params}" -ge 2 ]]; then
+  if [ "${number_of_params}" -ge 2 ]; then
     from_index="${2}"
     from_index_defined=1
-    if [[ "${number_of_params}" -ge 3 ]]; then
+    if [ "${number_of_params}" -ge 3 ]; then
       till_index="${3}"
       till_index_defined=1
     fi
@@ -43,19 +43,19 @@ function split_by_colon {
   i=0
   while true; do
     s="${array%%\:*}"
-    if [[ "${from_index_defined}" -eq 0 ]]; then
+    if [ "${from_index_defined}" -eq 0 ]; then
       echo "${s}"
     else
-      if [[ "${till_index_defined}" -ne 0 ]]; then
-        if [[ "${i}" -gt "${till_index}" ]]; then
+      if [ "${till_index_defined}" -ne 0 ]; then
+        if [ "${i}" -gt "${till_index}" ]; then
           break
         fi
       fi
-      if [[ "${i}" -ge "${from_index}" ]]; then
+      if [ "${i}" -ge "${from_index}" ]; then
         echo "${s}"
       fi
     fi
-    if [[ "${array}" = "${s}" ]]; then
+    if [ "${array}" = "${s}" ]; then
       break
     fi
     array="${array#*\:}"
@@ -83,7 +83,7 @@ function add_prefix_and_postfix {
   first=1
   result=""
   for s in $(split_by_colon "${array}"); do
-    if [[ "${first}" -eq 0 ]]; then
+    if [ "${first}" -eq 0 ]; then
       result="${result}:"
     fi
     result="${result}${prefix}${s}${postfix}"
@@ -107,7 +107,7 @@ function concat_all {
   first=1
   result=""
   for s in $(split_by_colon "${array}"); do
-    if [[ "${first}" -eq 0 ]]; then
+    if [ "${first}" -eq 0 ]; then
       result="${result}${delimiter}"
     fi
     result="${result}${s}"
@@ -119,7 +119,7 @@ function concat_all {
 function exist_any {
   array="${1}"
   for s in $(split_by_colon "${array}"); do
-    if [[ -f "${s}" ]] || [[ -d "${s}" ]]; then
+    if [ -f "${s}" ] || [ -d "${s}" ]; then
       echo "1:${s}"
       return
     fi
@@ -130,7 +130,7 @@ function exist_any {
 function exist_all {
   array="${1}"
   for s in $(split_by_colon "${array}"); do
-    if ! [[ -f "${s}" ]] && ! [[ -d "${s}" ]]; then
+    if ! [ -f "${s}" ] && ! [ -d "${s}" ]; then
       echo 0
       return
     fi
@@ -143,7 +143,7 @@ exit_code=0
 echo "Preparing application configuration at ${APPLICATION_CONFIGURATION_FILE}"
 j2 --import-env="" --filters "/app/bin/filters.py" -o "${APPLICATION_CONFIGURATION_FILE}" "/app/template/application.properties.j2"
 exit_code=$?
-if [[ "${exit_code}" -ne 0 ]]; then
+if [ "${exit_code}" -ne 0 ]; then
   echo "Failed to prepare application configuration at ${APPLICATION_CONFIGURATION_FILE}"
   echo "Exiting with ${exit_code}"
   exit "${exit_code}"
@@ -152,7 +152,7 @@ fi
 echo "Preparing JBoss EAP configuration at ${JBOSS_CONFIGURATION_FILE}"
 j2 --import-env="" --filters "/app/bin/filters.py" -o "${JBOSS_CONFIGURATION_FILE}" "/app/template/standalone.xml.j2"
 exit_code=$?
-if [[ "${exit_code}" -ne 0 ]]; then
+if [ "${exit_code}" -ne 0 ]; then
   echo "Failed to prepare JBoss EAP configuration at ${JBOSS_CONFIGURATION_FILE}"
   echo "Exiting with ${exit_code}"
   exit "${exit_code}"
@@ -184,53 +184,53 @@ failed=0
 timeout=0
 failed_marker=""
 while true; do
-  if [[ "$(pid_alive "${jboss_pid}")" -eq 0 ]]; then
+  if [ "$(pid_alive "${jboss_pid}")" -eq 0 ]; then
     alive=0
     break
   fi
   exist_any_result="$(exist_any "${fail_markers}")"
-  if [[ "$(get_nth_item 0 "${exist_any_result}")" -ne 0 ]]; then
+  if [ "$(get_nth_item 0 "${exist_any_result}")" -ne 0 ]; then
     failed=1
     failed_marker="$(get_nth_item 1 "${exist_any_result}")"
     break
   fi
-  if [[ "$(exist_all "${success_markers}")" -ne 0 ]]; then
+  if [ "$(exist_all "${success_markers}")" -ne 0 ]; then
     deployed=1
     break
   fi
   attempts=$((attempts+1))
-  if [[ "${attempts}" -ge "${deploy_check_attempts}" ]]; then
+  if [ "${attempts}" -ge "${deploy_check_attempts}" ]; then
     timeout=1
     break
   fi
   sleep "${DEPLOY_CHECK_INTERVAL}"
 done
 
-if [[ "${deployed}" -ne 0 ]]; then
+if [ "${deployed}" -ne 0 ]; then
   echo "Detected completion of deployment with $(concat_all ", " "${success_markers}")"
 else
-  if [[ "${failed}" -ne 0 ]]; then
+  if [ "${failed}" -ne 0 ]; then
     echo "Detected failed deployment with ${failed_marker}, stopping JBoss EAP"
     exit_code=1
   fi
-  if [[ "${timeout}" -ne 0 ]]; then
+  if [ "${timeout}" -ne 0 ]; then
     echo "Deployment timeout ${DEPLOY_TIMEOUT} sec happened, stopping JBoss EAP"
     exit_code=1
   fi
-  if [[ "${alive}" -ne 0 ]]; then
+  if [ "${alive}" -ne 0 ]; then
     kill -TERM "${jboss_pid}"
     alive=0
   fi
 fi
 
-if [[ "${alive}" -ne 0 ]]; then
+if [ "${alive}" -ne 0 ]; then
   wait "${jboss_pid}"
 fi
 
 trap - HUP INT QUIT PIPE TERM
 wait "${jboss_pid}"
 jboss_exit_code=$?
-if [[ "${exit_code}" -eq 0 ]]; then
+if [ "${exit_code}" -eq 0 ]; then
   exit_code="${jboss_exit_code}"
 fi
 
