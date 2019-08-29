710de1c593cc7672b59b0458f89eabd014e7018b
Marat Abrarov <abrarov@gmail.com>
2019-08-29 19:49:18 +0300

    Docker push retries

--- push-docker-images.sh
+++ push-docker-images.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "${DOCKERHUB_PASSWORD}" | docker login -u "${DOCKERHUB_USER}" --password-stdin
 
-mvn -f base-image/pom.xml docker:push
-mvn -f hollow-image/pom.xml docker:push
-mvn -f app-image/pom.xml docker:push
+mvn -f base-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
+mvn -f hollow-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
+mvn -f app-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
