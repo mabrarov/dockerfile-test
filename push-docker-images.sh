a04c89a92719e01578c743232ca60c61bc032479
Marat Abrarov <abrarov@gmail.com>
2019-08-29 20:42:24 +0300

    Retries when calling docker login

--- push-docker-images.sh
+++ push-docker-images.sh
@@ -2,8 +2,32 @@
 
 set -e
 
-echo "${DOCKERHUB_PASSWORD}" | docker login -u "${DOCKERHUB_USER}" --password-stdin
+# Copied from https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/travis_retry.bash
+travis_retry() {
+  local result=0
+  local count=1
+  while [[ "${count}" -le 3 ]]; do
+    [[ "${result}" -ne 0 ]] && {
+      echo -e "\\n${ANSI_RED}The command \"${*}\" failed. Retrying, ${count} of 3.${ANSI_RESET}\\n" >&2
+    }
+    "${@}" && { result=0 && break; } || result="${?}"
+    count="$((count + 1))"
+    sleep 1
+  done
 
-mvn -f base-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
-mvn -f hollow-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
-mvn -f app-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
+  [[ "${count}" -gt 3 ]] && {
+    echo -e "\\n${ANSI_RED}The command \"${*}\" failed 3 times.${ANSI_RESET}\\n" >&2
+  }
+
+  return "${result}"
+}
+
+main() {
+  travis_retry docker login -u "${DOCKERHUB_USER}" -p "${DOCKERHUB_PASSWORD}"
+
+  mvn -f base-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
+  mvn -f hollow-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
+  mvn -f app-image/pom.xml docker:push "-Ddocker.push.retries=${DOCKER_PUSH_RETRIES}"
+}
+
+main "${@}"
